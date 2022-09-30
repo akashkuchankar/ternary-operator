5f9103af70837792fdc2dbd84b7f48008175d6f8
akashkuchankar <kuchankarakash@gmail.com>
2022-09-30 06:44:57 -0700

    Add project files.

---
+++ ternary operator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ternary_operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10, b = 20;
+            int c = (a<b) ? a : b;
+            Console.WriteLine(c);
+        }
+    }
+}
